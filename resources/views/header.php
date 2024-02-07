51c884f745f0047d05d921843fac31a8257d6548
alliyahh <allyxgnzls@gmail.com>
2024-02-07 14:20:15 +0800

    fix: build errors, add: tailwind plugin

--- resources/views/header.php
+++ resources/views/header.php
@@ -4,11 +4,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@^2.0/dist/tailwind.min.css" rel="stylesheet">
+
     <title>header</title>
 </head>
 <body>
     <header>
-        <h1>Department of Health</h1>
-        <img src="../images/DOHlogo.png" alt="dohlogo">
+        <div class="bg-red-500 flex ">
+            <h1>Department of Health</h1>
+            <img src="../images/DOHlogo.png" alt="dohlogo">
+        </div>
+        
     </header>
-</body>
